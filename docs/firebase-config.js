0140971800c2c6c5a4deea5ae9a2f9f9a374371b
soy92103chiro-web <soy92103.chiro@gmail.com>
2026-06-19 11:16:01 +0900

    Add files via upload

---
+++ docs/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCMg_eCi6-c__rZ3kFpE0SJJDv-jCCHSLk",
+    authDomain: "progressguntttodo.firebaseapp.com",
+    projectId: "progressguntttodo",
+    storageBucket: "progressguntttodo.firebasestorage.app",
+    messagingSenderId: "61323305325",
+    appId: "1:61323305325:web:1cfd04770a52a06884ba64",
+    measurementId: "G-5GFTSGJQLS"
+};
